f4085c2b1cf8a7570e7a2c05e3cc7d1229e19acf
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2021-02-20 18:52:53 +0800

    add eslint, prettier, husky

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,8 @@
-import { AppProps } from 'next/app'
+import {AppProps} from 'next/app'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+	return <Component {...pageProps} />
 }
 
 export default MyApp
-
